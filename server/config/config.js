1a28af5e5282bf4434b474207243f922200c6828
Roberto Moya <rmoya@tradestation.com>
2020-03-21 00:02:41 -0600

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // Base de Datos
 // ==========================
 
-let urlDB = 'mongodb+srv://robmoya:O2o5rgCMvUVQEOgs@cluster0-smtpr.mongodb.net/cafe';
+let urlDB = process.env.MONGO_URI;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
