1acdaa32a3477deaaed47cf6834e1eec0b3e4130
Roberto Moya <rmoya@tradestation.com>
2020-03-27 00:49:38 -0600

    Productos y Categorias

--- server/routes/producto.js
+++ server/routes/producto.js
@@ -7,15 +7,17 @@ let app = express();
 let Producto = require('../models/producto');
 
 // Obtener Productos
-app.get('/productos', verificaToken, (req, res) => {
+app.get('/producto', verificaToken, (req, res) => {
     /*trae todos los productos
     populate usuario y categoria
     y paginado */
     let limite = Number(req.query.limite || 5);
+    let desde = Number(req.query.desde || 0);
     let condicion = { disponible: true };
     Producto.find(condicion)
         .populate('usuario', 'nombre email')
         .populate('categoria', 'descripcion')
+        .skip(desde)
         .sort('nombre')
         .limit(limite)
         .exec((err, productos) => {
@@ -36,33 +38,65 @@ app.get('/productos', verificaToken, (req, res) => {
 });
 
 // Obtener Productos por id
-app.get('/productos/:id', verificaToken, (req, res) => {
+app.get('/producto/:id', verificaToken, (req, res) => {
     /*populate usuario y categoria*/
     let id = req.params.id;
-    Producto.findById(id, (err, productoDB) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            });
-        }
-        if (!productoDB) {
+    Producto.findById(id)
+        .populate('usuario', 'nombre email')
+        .populate('categoria', 'descripcion')
+        .exec((err, productoDB) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+            if (!productoDB) {
+                return res.status(400).json({
+                    ok: true,
+                    err: {
+                        message: 'Producto no encontrado'
+                    }
+                });
+            }
             res.json({
                 ok: true,
-                err: { message: 'Producto no encontrada' }
+                producto: productoDB
             });
-        }
-        res.json({
-            ok: true,
-            producto: productoDB
-        });
 
-    });
+        });
 
 });
 
+//Buscar productos
+app.get('/producto/buscar/:termino', verificaToken, (req, res) => {
+
+    let termino = req.params.termino;
+
+    let regex = new RegExp(termino, 'i');
+
+    criterio = {
+        nombre: regex
+    };
+    Producto.find(criterio)
+        .populate('categoria', 'descripcion')
+        .exec((err, productos) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            res.json({
+                ok: true,
+                productos
+            });
+        });
+});
+
+
 // Crear un nuevo producto
-app.post('/productos', verificaToken, (req, res) => {
+app.post('/producto', verificaToken, (req, res) => {
     /*grabar un usuario, grabar una categoria*/
     let body = req.body;
     let producto = new Producto({
@@ -100,7 +134,7 @@ app.post('/productos', verificaToken, (req, res) => {
 });
 
 // actualizar un nuevo producto
-app.put('/productos/:id', verificaToken, (req, res) => {
+app.put('/producto/:id', verificaToken, (req, res) => {
     /*grabar un usuario, grabar una categoria*/
     let id = req.params.id;
     let body = req.body;
@@ -108,7 +142,9 @@ app.put('/productos/:id', verificaToken, (req, res) => {
     let cambios = {
         nombre: body.nombre,
         precioUni: body.precioUni,
-        descripcion: body.descripcion
+        descripcion: body.descripcion,
+        categoria: body.idCategoria,
+        disponible: body.disponible
     };
     Producto.findByIdAndUpdate(id, cambios, (err, productoDB) => {
         if (err) {
@@ -133,7 +169,7 @@ app.put('/productos/:id', verificaToken, (req, res) => {
 });
 
 // borrar un nuevo producto
-app.delete('/productos/:id', verificaToken, (req, res) => {
+app.delete('/producto/:id', verificaToken, (req, res) => {
     /*marcar el estado como borrado: disponible*/
     let id = req.params.id;
 
